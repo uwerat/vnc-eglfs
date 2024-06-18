915a4fa8a81439b04950d68c136b9eef22fc9dff
Uwe Rathmann <Uwe.Rathmann@tigertal.de>
2024-06-18 11:04:19 +0200

    old API ( never been called in "normal" situations ) implemented

--- platformproxy/VncProxyPlugin.cpp
+++ platformproxy/VncProxyPlugin.cpp
@@ -13,23 +13,50 @@ namespace
       public:
 
         QPlatformIntegration* create( const QString& system,
+            const QStringList& args ) override
+        {
+            // not used by QPlatformIntegrationFactory but who knows ...
+
+            int argc = 0;
+            char* argv = nullptr;
+
+            return createIntegration( system, args, argc, &argv );
+        }
+
+        QPlatformIntegration* create( const QString& system,
             const QStringList& args, int& argc, char** argv ) override
+        {
+            return createIntegration( system, args, argc, argv );
+        }
+
+      private:
+        QPlatformIntegration* createIntegration( const QString& system,
+            const QStringList& args, int& argc, char** argv )
         {
             QPlatformIntegration* integration = nullptr;
 
-            if ( system.startsWith( QStringLiteral("vnc"), Qt::CaseInsensitive ) )
+            const auto key = effectiveKey( system );
+            if ( !key.isEmpty() )
             {
                 const auto path = QString::fromLocal8Bit(
                     qgetenv( "QT_QPA_PLATFORM_PLUGIN_PATH" ) );
 
                 integration = QPlatformIntegrationFactory::create(
-                    system.mid( 3 ), args, argc, argv, path );
+                    key, args, argc, argv, path );
+
+                if ( integration )
+                    Vnc::setAutoStartEnabled( true );
             }
 
-            if ( integration )
-                Vnc::setAutoStartEnabled( true );
-
             return integration;
+        }
+
+        QString effectiveKey( const QString& system ) const
+        {
+            if ( system.startsWith( QStringLiteral("vnc"), Qt::CaseInsensitive ) )
+                return system.mid( 3 );
+
+            return QString();
         }
     };
 }
